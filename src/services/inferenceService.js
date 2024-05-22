228bdf2b5553d88b9c973f039f2c50e1ab6bcb04
Alim <teukualim6@gmail.com>
2024-05-22 19:52:13 +0700

    Push code to remote repository

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -12,31 +12,19 @@ async function predictClassification(model, image) {
     const prediction = model.predict(tensor);
     const score = await prediction.data();
     const confidenceScore = Math.max(...score) * 100;
+    let suggestion, label;
 
-    const classes = ['Melanocytic nevus', 'Squamous cell carcinoma', 'Vascular lesion'];
-
-    const classResult = tf.argMax(prediction, 1).dataSync()[0];
-    const label = classes[classResult];
-
-    let explanation, suggestion;
-
-    if (label === 'Melanocytic nevus') {
-      explanation = "Melanocytic nevus adalah kondisi permukaan kulit memiliki bercak warna yang berasal dari sel-sel melanosit, yakni pembentukan warna kulit dan rambut."
-      suggestion = "Segera konsultasi dengan dokter terdekat jika ukuran semakin membesar dengan cepat, mudah luka, atau berdarah."
+    if (score<0.5) {
+      label = "Non-cancer"
+      suggestion = "medical checkup berkala"
     }
   
-    if (label === 'Squamous cell carcinoma') {
-      explanation = "Squamous cell carcinoma adalah jenis kanker kulit yang umum dijumpai. Penyakit ini sering tumbuh pada bagian-bagian tubuh yang sering terkena sinar UV."
+    else {
+      label = "Cancer"
       suggestion = "Segera konsultasi dengan dokter terdekat untuk meminimalisasi penyebaran kanker."
     }
-  
-    if (label === 'Vascular lesion') {
-      explanation = "Vascular lesion adalah penyakit yang dikategorikan sebagai kanker atau tumor. Penyakit ini sering muncul pada bagian kepala dan leher."
-      suggestion = "Segera konsultasi dengan dokter terdekat untuk mengetahui detail terkait tingkat bahaya penyakit."
-  
-    }
 
-    return { confidenceScore, label, explanation, suggestion };
+    return { confidenceScore, label, suggestion };
   } catch (error) {
     throw new InputError(`Terjadi kesalahan input: ${error.message}`);
   }
